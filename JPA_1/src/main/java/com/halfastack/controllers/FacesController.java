52bec1e50c9b46d074aa0e8bb96e56be5b25f230
Marek Czernek <mczernek@redhat.com>
2018-03-30 22:47:51 +0200

    Prepare JDBC statements and basic backend, pending frontend

--- JPA_1/src/main/java/com/halfastack/controllers/FacesController.java
+++ JPA_1/src/main/java/com/halfastack/controllers/FacesController.java
@@ -1,30 +1,42 @@
 package com.halfastack.controllers;
 
-import javax.ejb.EJB;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.halfastack.db.BookDB;
+import com.halfastack.entities.Book;
 
 @RequestScoped
 @Named
 public class FacesController {
 	
-	String name;
-	String result;
-	String method;
+	private String name;
+	private String result;
+	private String method;
+	private List<Book> books;
+	private Logger log = LoggerFactory.getLogger(this.getClass());
 
 	public FacesController() {
 		result = "";
 	}
 	
 
-	@EJB
-	DBController controller;
+	@Inject
+	BookDB controller;
 	
 	public void find() {
 		if(name != null) {
-			result = controller.find(name, method);
+			books = controller.find(name, method);
+			log.info("Setting books: {}",books);
+			result = books.size() > 0 ? "" : "Your book/author was not. Try a different one.";
 		} else {
-			result = "Enter name or title please";
+			result = "Enter an author or a title please.";
 		}
 	}	
 	
@@ -46,5 +58,9 @@ public class FacesController {
 
 	public void setMethod(String method) {
 		this.method = method;
+	}
+
+	public List<Book> getBooks() {
+		return books;
 	}
 }
--- JPA_1/src/main/java/com/halfastack/db/BookDB.java
+++ JPA_1/src/main/java/com/halfastack/db/BookDB.java
@@ -1,8 +1,12 @@
 package com.halfastack.db;
 
+import java.util.Collections;
+import java.util.List;
+
 import javax.ejb.Singleton;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,17 +22,27 @@ public class BookDB {
 	EntityManager em;
 	
 	
-	
-	public String findByAuthor(String name) {
-		
-		log.info("am I null?");
-		log.info(Boolean.toString(em == null));
-		Book b = em.find(Book.class, 1L);
-		log.info(b.toString());
-		return "not implemented";
+	public List<Book> find(String name, String method) {
+		switch(method) {
+			case "author":
+				return findByAuthor(name);
+			case "title":
+				return findByTitle(name);
+			default:
+				return Collections.emptyList();
+		}
 	}
 	
-	public String findByTitle(String name) {
-		return "not implemented";
+	
+	public List<Book> findByAuthor(String name) {
+		TypedQuery<Book> query = em.createQuery("SELECT b FROM Book b WHERE lower(b.author) LIKE CONCAT('%',?1,'%')", Book.class);
+		query.setParameter(1, name.toLowerCase());
+		return query.getResultList();
+	}
+	
+	public List<Book> findByTitle(String title) {
+		TypedQuery<Book> query = em.createQuery("SELECT b FROM Book b WHERE lower(b.title) LIKE CONCAT('%',?1,'%')", Book.class);
+		query.setParameter(1, title.toLowerCase());
+		return query.getResultList();
 	}
 }
