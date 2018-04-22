4e33d321752b6d088eabb5472f684631af7f9ebd
Marek Czernek <mczernek@redhat.com>
2018-04-22 15:16:00 +0200

    Add OneToMany mapping SQL

--- JPA_1/src/main/resources/META-INF/load.sql
+++ JPA_1/src/main/resources/META-INF/load.sql
@@ -1,4 +1,8 @@
-INSERT INTO LIBRARY("ID","TITLE","AUTHOR","AVAILABLE") values (1,'The Cat in the Coffin','Koike, Mariko',true);
-INSERT INTO LIBRARY("ID","TITLE","AUTHOR","AVAILABLE") values (2,'The Buried Giant','Ishiguro, Kazuo',true);
-INSERT INTO LIBRARY("ID","TITLE","AUTHOR","AVAILABLE") values (3,'Battle Hymn of the Tiger Mother','Chua, Amy',true);
-INSERT INTO LIBRARY("ID","TITLE","AUTHOR","AVAILABLE") values (4,'The Quiet American','Greene, Graham',false);
+INSERT INTO AUTHOR("ID","SECONDNAME","FIRSTNAME") values (1,'Koike', 'Mariko');
+INSERT INTO AUTHOR("ID","SECONDNAME","FIRSTNAME") values (2,'Ishiguro', 'Kazuo');
+INSERT INTO AUTHOR("ID","SECONDNAME","FIRSTNAME") values (3,'Chua', 'Amy');
+INSERT INTO AUTHOR("ID","SECONDNAME","FIRSTNAME") values (4,'Greene', 'Graham');
+INSERT INTO LIBRARY("ID","TITLE","AUTHOR","AVAILABLE") values (1,'The Cat in the Coffin', 1, true);
+INSERT INTO LIBRARY("ID","TITLE","AUTHOR","AVAILABLE") values (2,'The Buried Giant', 2, true);
+INSERT INTO LIBRARY("ID","TITLE","AUTHOR","AVAILABLE") values (3,'Battle Hymn of the Tiger Mother', 3, true);
+INSERT INTO LIBRARY("ID","TITLE","AUTHOR","AVAILABLE") values (4,'The Quiet American', 4, false);
