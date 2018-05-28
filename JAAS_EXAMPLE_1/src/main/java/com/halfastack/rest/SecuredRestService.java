97bdfe92c62c9eff56f68cee772c306e1c83b875
Marek Czernek <mczernek@redhat.com>
2018-05-28 17:58:04 +0200

    Expand roles in the rest application and add comments in web.xml

--- JAAS_EXAMPLE_1/src/main/java/com/halfastack/rest/SecuredRestService.java
+++ JAAS_EXAMPLE_1/src/main/java/com/halfastack/rest/SecuredRestService.java
@@ -1,24 +1,36 @@
 package com.halfastack.rest;
 
+import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 
 @Path("/jaas")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 @Stateless
 public class SecuredRestService {
 	
 	@RolesAllowed({"developer"})
 	@GET
-	@Path("")
-	public String getHello() {
-		return "{\"response\":\"Hello from JAAS\"}";
+	@Path("/getDeveloperHello")
+	public String getDeveloperHello() {
+		return "Hello for developers";
 	}
+	
+	@RolesAllowed("guest")
+	@GET
+	@Path("/getGuestOnlyHello")
+	public String getGuestExclusiveHello() {
+		return "Hello for guests";
+	}
+	
+	@PermitAll
+	//@RolesAllowed({"guest","developer"})
+	@GET
+	@Path("/getHello")
+	public String getHello() {
+		return "Hello for guests and developer";
+	}
+	
+	
 }
